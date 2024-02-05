d4244fb7f7ec9670ee91ac83384ca4c10e2fcb3a
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2024-02-05 05:38:34 -0300

    Update Routes.php

--- src/Services/Routes.php
+++ src/Services/Routes.php
@@ -2,10 +2,16 @@
 
 namespace TasksManager\Services;
 
+/**
+ * @OA\Info(title="Minha API", version="1.0")
+ */
+
 use Slim\App;
 use Slim\Exception\HttpNotFoundException;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Routing\RouteCollectorProxy;
+
 use TasksManager\Controllers\TaskController;
 use TasksManager\Controllers\TaskGroupController;
 
@@ -13,6 +19,13 @@ class Routes
 {
     public static function defineRoutes(App $app): void
     {
+        $app->get('/', function (Request $request, Response $response) {
+            $response->getBody()->write('Tela principal');
+            return $response
+                ->withHeader('Content-Type', 'application/json');
+
+        });
+
         $app->group('/tasks', function (RouteCollectorProxy $group) {
             $group->get('', [TaskController::class, 'getAll']);
             $group->post('', [TaskController::class, 'create']);
